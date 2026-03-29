29b7c341623233f18fc60b1dd77a91e904707b5a
Naman80 <naman.goel80@gmail.com>
2026-03-29 19:03:27 +0530

    chore: structure and ts added

--- src/langchainExample.ts
+++ src/langchainExample.ts
@@ -1,12 +1,9 @@
 import * as z from "zod";
-import { configDotenv } from "dotenv";
 import { tool } from "langchain";
 import { ChatOpenRouter } from "@langchain/openrouter";
 
-configDotenv();
-
-const OPEN_ROUTER_API_KEY = process.env.OPEN_ROUTER_API_KEY;
-const MODEL_NAME = process.env.MODEL_NAME;
+export const OPEN_ROUTER_API_KEY = process.env.OPEN_ROUTER_API_KEY ?? "temp";
+export const MODEL_NAME = process.env.MODEL_NAME ?? "temp";
 
 const getWeather = tool(
   (city) => {
@@ -18,17 +15,13 @@ const getWeather = tool(
     schema: z.object({
       city: z.string(),
     }),
-    responseFormat: z.object({
-      punny_response: z.string(),
-      weather_conditions: z.string().optional(),
-    }),
   },
 );
 
 const modelWithTools = new ChatOpenRouter({
-  model: MODEL_NAME,
+  model: MODEL_NAME!,
   temperature: 0,
-  apiKey: OPEN_ROUTER_API_KEY,
+  apiKey: OPEN_ROUTER_API_KEY!,
 }).bindTools([getWeather]);
 
 const aiMsg = await modelWithTools.invoke("whats the weather in new delhi");
